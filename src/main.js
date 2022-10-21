156d66c6555de2ff3ce8bfd823bff7a256af31ed
hiroro <110441714+hiroro-d@users.noreply.github.com>
2022-10-22 00:27:17 +0900

    pwaを先に居れておいた

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import '@/tailwind.css'
 import store from './store'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
